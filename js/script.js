d412b813bea2011042c2760ea3a54617da068268
Yoshihiro Mizumachi <arikkusu77@gmail.com>
2023-04-24 20:00:48 +0900

    20230424にコミット

--- js/script.js
+++ js/script.js
@@ -1,4 +1,36 @@
 $(function() {
   // drawer.js
   $( '.drawer' ).drawer()
+
+  // smoothscroll
+  // #から始まるURLがクリックされた時
+  jQuery('a[href^="#"]').click(function() {
+    // 移動速度を指定(ミリ秒)
+    let speed = 300;
+    // hrefで指定されたidを取得
+    let id = jQuery(this).attr("href");
+    // idの値が#のみだったらターゲットをhtmlタグにしてトップに戻るようにする
+    let target = jQuery("#" == id ? "html" : id);
+    // ページのトップを基準にターゲットの位置を取得
+    let position = jQuery(target).offset().top;
+    // ターゲットの位置までspeedの速度で移動
+    jQuery("html, body").animate(
+      {
+        scrollTop: position - $( '#js-header' ).outerHeight()
+      },
+      speed
+    );
+    return false;
+  });
+
+  // wowjs
+  new WOW().init()
 })
+
+
+
+
+
+
+
+
