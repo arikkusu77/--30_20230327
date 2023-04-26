7047c5c0a670242c6c3b703d0a5ad7dbd4d6f5b6
Yoshihiro Mizumachi <arikkusu77@gmail.com>
2023-04-27 06:25:07 +0900

    20230427にコミット

--- js/script.js
+++ js/script.js
@@ -25,6 +25,49 @@ $(function() {
 
   // wowjs
   new WOW().init()
+
+  // googleform
+  let $form = $( '#js-form' )
+  $form.submit(function () {
+    $.ajax({
+      url: $form.attr('action'),
+      data: $form.serialize(),
+      type: "POST",
+      dataType: "xml",
+      statusCode: {
+        0: function () {
+          // 送信に成功したときの処理
+          $form.slideUp()
+          $( '#js-success' ).slideDown()
+        },
+        200: function() {
+          // 送信に失敗したときの処理
+          $form.slideUp()
+          $( '#js-error' ).slideDown()
+        }
+      }
+    });
+
+    return false;
+  });
+
+  // フォームの入力確認
+  let $submit = $( 'js-submit' )
+  $( '#js-form, #js-submit textarea' ).on( 'change', function() {
+    if(
+      $( '#js-form input[type="text"]' ).value !== "" &&
+      $( '#js-form input[type="email"]' ).value !== "" &&
+      $( '#js-form input[name="entry.1571057677"]' ).prop( 'checked' ) === true
+    ) {
+      //  全て入力されたとき
+      $submit.prop( 'disabled', false )
+      $submit.addClass( '-active' )
+    } else {
+      //  入力されていないとき
+      $submit.prop( 'disabled', true )
+      $submit.removeClass( '-active' )
+    }
+  });
 })
 
 
